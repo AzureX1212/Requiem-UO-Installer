808f90e666463c4e9649bb03495a4031f309bd7d
Tony Avila <tony.e.avila@gmail.com>
2017-07-29 02:12:41 -0500

    Tweaked script to remove zenity dependency, automated more of the tweaks needed to run the client.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RequiemUOLinuxScript
-Script created for installing RequiemUO Client on Linux with Wine
+Script created for installing RequiemUO Client on Linux with Wine.
+
+This is a script created by AzureX for semi-automated installs of Ultima Online thru Linux with Wine.
+It is primarily designed to work with the requirements of Requiem Shard.
--- requiem-install.sh
+++ requiem-install.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 #---------------------------------------
 #      RequiemUO Linux Installer
-# This is a test script to install RequiemUO using Wine and 
+# This is a test script to install RequiemUO using Wine and
 # and some of it's extra libraries. The goal is to make an
 # easy method of installation, thats also not very complex to
-# maintain. 
+# maintain.
 #
 # This is a side project of AzureX and thus is not *officially* supported
 # by RequiemUO staff.
 #
 # DO NOT REPORT ANY ISSUES TO ANY STAFF MEMBER.
+# YOUR ANIMUS == 0.
 #
 # REPORT BUGS VIA THIS GITHUB PAGE:
 #
 #
 
 # Declaring basic variables I'll need to run the script
-
 install_dir=$HOME/.RequiemUO
 cache_dir=$HOME/.cache/RequiemUO
-
-wine_setup() {
-echo "Installing to $install_dir"
+# and lets set the prefix and arch variable for the entire script
 export WINEPREFIX=$HOME/.RequiemUO
 export WINEARCH=win32
 
-zenity --info --text "Select Windows 2003 at the bottom of the following dialog, then apply"
-
-winecfg
+wine_setup() {
+echo "Installing to $install_dir"
+echo "We will install into $WINEPREFIX"
 }
 
 dir_setup() {
+#This will initialize our directory for storing the downloaded files for install, we keep it to speed up the script for reinstalls
 mkdir -p $cache_dir
 }
 
 dl_files() {
-
+#Check and download all our our missing files from verified sources. These may be changed later on if we need new sources.
 if [ -f "$cache_dir/UO.exe" ]
 then
 	echo "UO.exe Found!"
@@ -56,10 +55,10 @@ then
 else
 	curl -o $cache_dir/ReqAutoPatcher_Setup.exe http://www.13thrones.com/files/ReqAutoPatcher_Setup.exe
 fi
-
 }
 
 winetricks() {
+#Setup winetricks from official source, as this is better updated than most distro versions, and installs required dotnet45 library for launcher.
 if [ -f "$cache_dir/winetricks" ]
 then
 	echo "Oh hey look! I found winetricks!"
@@ -67,32 +66,20 @@ else
 	curl -o $cache_dir/winetricks https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 	chmod +x $cache_dir/winetricks
 fi
-
-mkdir -p $HOME/.RequiemUO/share/wine/gecko
-
-curl -o $HOME/.RequiemUO/share/wine/gecko/ "http://dl.winehq.org/wine/wine-gecko/2.40/wine_gecko-2.40-x86.msi"
-
-WINEPREFIX=$HOME/.RequiemUO $cache_dir/winetricks -q dotnet45 
+$cache_dir/winetricks -q dotnet45 > /dev/null
+$cache_dir/winetricks windowmanagerdecorated=n > /dev/null
+$cache_dir/winetricks win7 > /dev/null #sets our wine version over to windows 7 for the launcher
 }
 
-#copy_files() {
-#mkdir -p "$HOME/.RequiemUO/drive_c/Program Files/RequiemUO"
-#cp -a $cache_dir "$HOME/.RequiemUO/drive_c/Program Files/"
-#}
-
 install_UO () {
-zenity --info --text "We are now going to install UO, hold on to your butts!"
-WINEPREFIX=$HOME/.RequiemUO wine "$cache_dir/UO.exe"
-WINEPREFIX=$HOME/.RequiemUO wine "$cache_dir/UOSteam.exe"
-zenity --info --text "Please set your windows version to Windows 7 in the next prompt, then apply."
-winecfg
+wine "$cache_dir/UO.exe"
+wine "$cache_dir/UOSteam.exe"
 }
 
 patch_UO () {
-zenity --info --text "Now we are going to install and run the patcher! Once it is installed leave it to luanch it and setup the patcher as per the website. Once you have patched the game close the patcher"
-WINEPREFIX=$HOME/.RequiemUO wine "$cache_dir/ReqAutoPatcher_Setup.exe"
+wine "$cache_dir/ReqAutoPatcher_Setup.exe" > /dev/null
+read -p "Press enter once patcher is completed..."
 }
-
 final_install () {
 
 /bin/cat <<EOM > $HOME/.RequiemUO/uolaunch
@@ -124,9 +111,15 @@ Terminal=false
 Comment="Launch UOSteam to play Requiem UO"
 Categories=Games
 EOM
-zenity --info --text "Good News! We are done! You should now be able to launch and patch UO from your applications menu!"
-} 
-
+}
+clear
+echo "You must Understand the following rules:"
+echo "1. This is an EXPERIMENTAL script"
+echo "2. Do not post issues with the script to development team"
+echo "3. This is not a native souliton, it does require wine"
+echo "4. This is not a stable solution."
+read -p "Press enter when you're ready to proceed..."
+clear
 wine_setup
 dir_setup
 dl_files
